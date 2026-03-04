4f3d5c9086781d4dd349f21a93f1ce7a7e6de41f
Awwal Abedayo <adebayoawwaldele@gmail.com>
2026-03-04 20:33:20 +0100

    Add project files.

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,43 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Catergory> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Catergory obj)
+        {
+            if(obj.Name == obj.DiplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly watch the Name.");
+            }
+            if(obj.Name!=null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is an invaild value"); 
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index"  );
+        }
+    }
+}
---
+++ BulkyWeb/Models/Catergory.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Catergory
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Name{ get; set; }
+        [Range(1,100)]
+        public int DiplayOrder { get; set; }
+    }
+}
