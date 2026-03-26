0f5aa8b27f8aa321b42d6466c82042259912941f
Awwal Abedayo <adebayoawwaldele@gmail.com>
2026-03-26 17:36:13 +0100

    add

---
+++ BulkyDataAccess/Repositry/IRepositry/IRepostiry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyDataAccess.Repositry.IRepositry
+{
+    internal interface IRepostiry<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetFirstOrDefault();
+    }
+}
