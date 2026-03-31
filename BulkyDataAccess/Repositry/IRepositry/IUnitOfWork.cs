17c414693ebb104885b028b3c7f0ce142e30cf21
Awwal Abedayo <adebayoawwaldele@gmail.com>
2026-03-31 19:17:00 +0100

    add

---
+++ BulkyDataAccess/Repositry/IRepositry/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyDataAccess.Repositry.IRepositry
+{
+    public interface IUnitOfWork
+    {
+        ICategoryRepositry Category{ get; }
+        IProductRepositry Product { get; }
+
+        void Save();
+    }
+}
---
+++ BulkyDataAccess/Repositry/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using BulkyDataAccess.Repositry.IRepositry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyDataAccess.Repositry
+{
+    public class UnitOfWork: IUnitOfWork
+    {
+        private ApplicationDbContext _db;
+         public ICategoryRepositry Category{ get; private set; }
+        public IProductRepositry Product { get; private set; }
+        public UnitOfWork (ApplicationDbContext db)
+        {
+            _db = db;
+            Category =new CategoryRepository(_db);
+            Product = new ProductRepositry(_db);
+        }
+
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
